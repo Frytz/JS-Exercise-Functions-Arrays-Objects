bdfd617f5e21200e4aba7952fab3b40680d7e9d5
John Rossi <Dacook1983@gmail.com>
2020-01-26 12:58:56 -0500

    getName passed

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function makePersonObject(id, name, email) {
         id: id,
         name: name,
         email: email
-    }
+    };
 }
 
 /**
@@ -60,7 +60,10 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
  */
-function getName( /* code here */ ) {
+function getName(name) {
+
+    var name = 'Hello, my name is Luke';
+    return name;
     /* code here */
 }
 
